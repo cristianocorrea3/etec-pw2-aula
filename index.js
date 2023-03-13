3d461440c928ae83df08c72f9c1a5f39afec3a19
cristianocorrea3 <66446324+cristianocorrea3@users.noreply.github.com>
2023-03-13 16:34:12 -0300

    PW2 initial commit.
    PW2 initial commit.

---
+++ index.js
@@ -0,0 +1,41 @@
+/*IMPORT DO PACOTE EXPRESS*/
+const express = require('express');
+
+/*INSTANCIA EXECUTAVEL DO EXPRESS*/
+const app = express();
+
+/*HABILITA A APLICAÇÃO A MANIPULAR JSON*/
+app.use(express.json());
+
+/*HABILITA A APLICAÇÃO A MANIPULAR DADOS DE UM CORPO DE DADOS*/
+app.use(express.urlencoded({extended:true}));
+
+/*
+TESTE DE ROTA HTTP GET
+2 PARAMETROS:
+    1 - NOME DA ROTA
+    2 - CALLBACK QUE EXECUTA A AÇÃO DA ROTA
+*/
+// app.get('/testeGET', (req, res)=>{
+//     res.send('RESPOSTA DA ROTA DE HTTP GET');
+// });
+// app.post('/testePOST', (req, res)=>{
+//     res.send('RESPOSTA DA ROTA DE HTTP POST');
+// });
+// app.put('/testePUT', (req, res)=>{
+//     res.send('RESPOSTA DA ROTA DE HTTP PUT');
+// });
+// app.delete('/testeDELETE', (req, res)=>{
+//     res.send('RESPOSTA DA ROTA DE HTTP DELETE');
+// });
+
+/*
+CRIAÇÃO DO SERVIDOR HTTP:
+PARAMETROS:
+1 - PORTA LÓGICA
+2 - CALLBACK: 
+*/
+
+app.listen(3000, ()=>{ 
+    console.log('SERVIDOR RODANDO EM - http://localhost:3000'); 
+});
