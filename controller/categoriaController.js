f2f4067cd964146f00edd059c6c50c6037ecc1ab
cristianocorrea3 <cristiano.moraes4@etec.sp.gov.br>
2023-04-24 22:24:41 -0300

    Finalização do CRUDde categoria

--- controller/categoriaController.js
+++ controller/categoriaController.js
@@ -5,7 +5,7 @@ const categoriaModel = require('../model/categoriaModel');
 /* GERENCIADOR DE ROTAS*/
 const router = express.Router();
 
-/* ROTA DE INSERÇÃO DE AUTOR(POST)*/
+/* ROTA DE INSERÇÃO DE CATEGORIA(POST)*/
 router.post('/categoria/inserir', (req,res)=>{
 
     let nome_categoria = req.body.nome_categoria;
@@ -14,13 +14,20 @@ router.post('/categoria/inserir', (req,res)=>{
         {nome_categoria}
     ).then(
         ()=>{
-            res.send('CATEGORIA INSERIDA')
+           return res.status(201).json({
+            errorStatus:false,
+            mensageStatus:'CATEGORIA INSERIDA COM SUCESSO'
+           });
         }   
-    )
-
-    //res.send('ROTA DE CATEGORIA DE INSERÇÃO!');
-
-})
+    ).catch(
+        (error)=>{
+            return res.status(500).json({
+                errorStatus:true,
+                mensageStatus: error
+            });
+        }
+    );
+});
 
 /* ROTA DE SELEÇÃO DE AUTOR(GET)*/
 router.get('/categoria/selecionar', (req,res)=>{
@@ -31,9 +38,16 @@ router.get('/categoria/selecionar', (req,res)=>{
             //console.log(categorias);
             res.json(categorias);
         }
-    )
+    ).catch(
+        (error)=>{
+            return res.status(500).json({
+                errorStatus:true,
+                mensageStatus: error
+            });
+        }
+    );
 
-})
+});
 
 /* ROTA DE ALTERAÇÃO DE AUTOR(PUT)*/
 router.put('/categoria/alterar', (req,res)=>{
@@ -46,19 +60,45 @@ router.put('/categoria/alterar', (req,res)=>{
         {where:{id}}
     ).then(
         ()=>{
-            res.send("CATEGORIA ALTERADA")
+            return res.status(200).json({
+                errorStatus:false,
+                mensageStatus:'CATEGORIA ALTERADA COM SUCESSO'
+            });
         }
-    )
+    ).catch(
+        (error)=>{
+            return res.status(500).json({
+                errorStatus:true,
+                mensageStatus: error
+            });
+        }
+    );
 
-    //
-    res.send('ROTA DE CATEGORIA DE ALTERAÇÃO!');
-
-})
+});
 
 /* ROTA DE EXCLUSÃO DE AUTOR(DELETE)*/
-router.delete('/categoria/excluir', (req,res)=>{
-    res.send('ROTA DE CATEGORIA DE DELETE!');
+router.delete('/categoria/excluir/:id', (req,res)=>{
+    
+    let id = req.params.id;
+    console.log('ID: ' + id);
 
-})
+    categoriaModel.destroy(
+        {where:{id}}
+    ).then(
+        ()=>{
+            return res.status(200).json({
+                errorStatus:false,
+                mensageStatus:'CATEGORIA EXCLUIDA COM SUCESSO'
+               });
+        }
+    ).catch(
+        (error)=>{
+            return res.status(500).json({
+                errorStatus:true,
+                mensageStatus: error
+            });
+        }
+    );
+});
 
 module.exports = router;
